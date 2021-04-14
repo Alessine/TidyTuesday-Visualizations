1fad486348fd0d908051badd97573e9cb9264b2a
Angela Niederberger <angelaniederberger@protonmail.ch>
2021-04-14 22:09:21 +0200

    trying out animation

--- 2021W16_US_post_offices/210413_tidytuesday_uspostoffices.R
+++ 2021W16_US_post_offices/210413_tidytuesday_uspostoffices.R
@@ -3,6 +3,7 @@ library(glue)
 library(RColorBrewer)
 library(tidyverse)
 library(maps)
+library(gganimate)
 
 # Read in the data from the tidy tuesday library.
 tuesdata <- tidytuesdayR::tt_load(2021, week = 16)
@@ -16,14 +17,31 @@ post_offices_1950 <- df_us_postoffices %>%
            between(post_offices.longitude, -126, -60) &
            (post_offices.discontinued > 1950 | is.na(post_offices.discontinued)))
 
+df_us_postoffices_2 <- df_us_postoffices %>%
+  filter(between(post_offices.longitude, -126, -60))
+
 theme_set(theme_void())
 gg1 <- ggplot() +
   geom_polygon(data = states, aes(x = long, y = lat, group = group), fill = "midnightblue", color = "darkgrey") +
   coord_fixed(1.3)
 
-gg1 +
+plot_1950 <- gg1 +
   geom_point(data = post_offices_1950, aes(x = `post_offices.longitude`, y = `post_offices.latitude`), color = "yellow", size = 0.01) +
   labs(title = "Post Offices in mainland U.S. in 1950") +
   theme(plot.title = element_text(hjust = 0.08, margin = margin(t = -50, b = -10)))
+
+gg2 <- ggplot() +
+  geom_polygon(data = states, aes(x = long, y = lat, group = group), fill = "midnightblue", color = "darkgrey") +
+  coord_fixed(1.3)
+
+plot_est <- gg2 +
+  geom_point(data = df_us_postoffices_2, aes(x = `post_offices.longitude`, y = `post_offices.latitude`), color = "yellow", size = 0.01) +
+  labs(title = "Post Offices in mainland U.S.") +
+  theme(plot.title = element_text(hjust = 0.08, margin = margin(t = -50, b = -10))) +
+  transition_manual(`post_offices.established`, cumulative = TRUE)
+
+anim <- animate(plot_est)
+
+anim_save("US_postoffices_est.gif", anim)
 
 
